41551a923b2a4fc6e4e2d10d137820879a2f0c16
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-10 11:12:45 +0100

    implemented routing to each 3 inital components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// pages & components
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+        </Switch>
+      </BrowserRouter>
 
     </div>
   );
