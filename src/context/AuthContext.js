b4a099237d1a6b23bf232e7a6dd5625663f5058f
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-10 22:08:01 +0100

    now deal with user logging out, user set to null

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,6 +7,8 @@ export const authReducer = (state, {type, payload}) => {
     switch(type){
         case 'LOGIN':
             return { ...state, user: payload }
+        case 'LOGOUT':
+            return {...state, user: null}
         default:
             return state
     }
