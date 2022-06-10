1a44d8fd8efb1990764f8c6872f5a74e9e2b0577
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-10 22:28:33 +0100

    added new li to navbar to logout user via useLogout hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useLogout} from '../hooks/useLogout'
 
 import styles from './Navbar.module.css'
 
 export default function Navbar() {
+  const {logout } = useLogout()
   return (
     <nav className={styles.navbar}>
         <ul>
             <li className={styles.title}><Link to='/'>myMoney</Link></li>
             <li><Link to='/login'>Login</Link></li>
             <li><Link to='/signup'>Signup</Link></li>
+            <li className='btn' onClick={logout}>Logout</li>
         </ul>
     </nav>
   )
