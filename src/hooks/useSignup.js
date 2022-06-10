d6f09d3615e59cd854de50631612d12bb2a1f968
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-10 21:58:23 +0100

    use dispatch func to update context

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import { projectAuth } from "../firebase/config"
+import {useAuthContext} from './useAuthContext'
 
 export function useSignup() {
 
     const [error, setError] = useState(null);
     const [isPending, setIsPending] = useState(false);
-
+    const {dispatch} = useAuthContext()
 
     const signup = async (email, password, displayName) => {
         setError(null);
@@ -13,7 +14,6 @@ export function useSignup() {
 
         try {
             const res = await projectAuth.createUserWithEmailAndPassword(email, password);
-            console.log(res.user);
 
             if(!res){
                 throw new Error('Could not complete signup');
@@ -22,8 +22,12 @@ export function useSignup() {
             // add display name to user
             await res.user.updateProfile({displayName});
 
+            // dispatch login action
+            dispatch({type: 'LOGIN', payload: res.user})
+            
+
             setIsPending(false);
-            setError(false);
+            setError(null);
         
         } 
         catch (err) {
