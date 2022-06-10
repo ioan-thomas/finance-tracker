54443e4013ce4bc9746383a422729b4ce36ea4f5
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-10 11:50:45 +0100

    Added navbar with styles and added to document flow

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
+import Navbar from './components/Navbar';
 
 import './App.css'
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <Switch>
             <Route exact path="/">
               <Home />
