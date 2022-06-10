188679c52203fdeb3acbfdcd271b1509b52f7163
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-10 15:57:45 +0100

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import {firebase} from 'firebase-app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCr81sMcPuxGF5_bn8C2bk5chn5a9Q3KtU",
+    authDomain: "mymoney-74ac4.firebaseapp.com",
+    projectId: "mymoney-74ac4",
+    storageBucket: "mymoney-74ac4.appspot.com",
+    messagingSenderId: "927934089224",
+    appId: "1:927934089224:web:7619e7c07b20d08b28e9bd"
+  };
+
+//   init firebase
+
+firebase.initializeApp(firebaseConfig)
+
+// init service 
+
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -9,6 +9,9 @@ export default function Signup() {
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(email, password, displayName)
+    setDisplayName('')
+    setEmail('')
+    setPassword('')
   }
 
   return (
