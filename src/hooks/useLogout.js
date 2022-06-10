6ff3f547e91b1834df43412c9a47421fbd2f48cd
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-10 22:09:04 +0100

    logout hook signs user out firebase, sending logout through dispatch, returns object

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { projectAuth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext"; 
+// the above is used to import dispatch func 
+
+export function useLogout() {
+    const [ error, setError ] = useState(null);
+    const [ isPending, setIsPending ] = useState(false);
+    const { dispatch } = useAuthContext();
+
+    const logout = async () => {
+        setError(null);
+        setIsPending(true);
+
+        // try sign user out 
+        try {
+            await projectAuth.signOut();
+
+            // dispatch logout action
+
+            dispatch({type: 'LOGOUT'})
+            setIsPending(false);
+            setError(null);
+        } 
+        catch (err) {
+            console.log(err.message);
+            setError(err.message);
+            setIsPending(false);
+            
+        }
+    }
+  return {logout, error, isPending}
+}
