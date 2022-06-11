647e37ff43716d14cb9cabadf6f14f098059ba0a
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-11 14:23:22 +0100

    conditionally show components based on if user context is true

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import { AuthContextProvider } from '../context/AuthContext'
 import { Link } from 'react-router-dom'
 import {useLogout} from '../hooks/useLogout'
 
 import styles from './Navbar.module.css'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 export default function Navbar() {
+  const {user} = useAuthContext()
   const {logout } = useLogout()
   return (
     <nav className={styles.navbar}>
         <ul>
             <li className={styles.title}><Link to='/'>myMoney</Link></li>
-            <li><Link to='/login'>Login</Link></li>
-            <li><Link to='/signup'>Signup</Link></li>
-            <li className='btn' onClick={logout}>Logout</li>
+
+            {!user && (
+              <>
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/signup'>Signup</Link></li>
+              </>
+            )}
+            {user && (
+              <>
+                <li>Hello, {user.displayName}</li>
+                <li className='btn' onClick={logout}>Logout</li>
+              </>
+            )}
         </ul>
     </nav>
   )
