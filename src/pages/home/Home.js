ef4e7e2a8898d7214d2c9705cb8cd07135a36568
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-12 17:23:00 +0100

    passed user id as props to TransactionFrom component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,16 @@
 // styles
 import styles from './Home.module.css'
-// components
+// components & hooks
 import { TransactionForm } from './TransactionForm'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 export default function Home() {
+  const {user} = useAuthContext();
   return (
     <div className={styles.container}>
       <div className={styles.content}>Transaction list</div>
-      <div className={styles.sidebar}>
-        <TransactionForm/>
+      <div className={styles.sidebar} >
+        <TransactionForm uid={user.uid}/>
       </div>
     </div>
   )
