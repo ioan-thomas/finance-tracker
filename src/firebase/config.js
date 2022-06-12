0d3e70e6508debffc632313dd45381b2a3a7c33a
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-12 17:02:24 +0100

    created a timestamp var from firebase and exported it

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,7 +18,7 @@ firebase.initializeApp(firebaseConfig)
 // init service 
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
+const timestamp = firebase.firestore.Timestamp
 
 
-
-export { projectFirestore, projectAuth }
+export { projectFirestore, projectAuth, timestamp }
