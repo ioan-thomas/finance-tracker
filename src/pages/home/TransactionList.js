a04e467d4b5c22573ceb27cfbcb5d0ad49b72614
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-13 11:55:48 +0100

    using the useFirestore hook to delete doc on click of button

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,12 +1,18 @@
+import { useFirestore } from '../../hooks/useFirestore'
+
+
 import styles from './Home.module.css'
 
 export function TransactionList({transactions}) {
+    const {deleteDocument, response} = useFirestore('transactions');
+    console.log(response)
   return (
         <ul className={styles.transactions}>
             {transactions.map(transaction => {
                 return (<li key={transaction.id}>
                     <p className={styles.name}>{transaction.name}</p>
                     <p className={styles.amount}>${transaction.amount}</p>
+                    <button onClick={() => deleteDocument(transaction.id)}>X</button>
                 </li>)}
             )}
         </ul>
